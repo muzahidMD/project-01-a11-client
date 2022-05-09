15119b756d2d400060915d895b3162964f76fbb7
Muzahidul Islam <mujahidul.core@gmail.com>
2022-05-09 17:36:13 +0600

    added registre page and set .env.local fife

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 import Footer from './Pages/shared/Footer/Footer';
 import Header from './Pages/shared/Header/Header';
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/home' element={<Home />}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Login.css'
 
-const Login = () => {
+const Register = () => {
     return (
         <div className='container login-form'>
-            <h2 className='my-5 login-title'>Please login</h2>
+            <h2 className='my-5 login-title'>Please Register</h2>
             <form className='w-50 mx-auto'>
+                <input type="text" name="name" id="" placeholder='Name' required />
                 <input type="text" name="email" id="" placeholder='Email' required />
                 <input type="password" name="password" id="" placeholder='Password' required />
                 <input className='w-50 d-block mx-auto' style={{ backgroundColor: "#00896F", color: "#ffffff" }} type="submit" value="Login" />
             </form>
             <div className='text-center'>
-                <p>Don't have an account ? <span><Link to="/register" style={{ color: "#00896F" }} className='text-decoration-none pe-auto'>Please Register</Link></span></p>
+                <p>Already have an account ? <span style={{ color: "#00896F" }}><Link to="/login" style={{ color: "#00896F" }} className='text-decoration-none pe-auto'>Please Login</Link></span></p>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default Register;

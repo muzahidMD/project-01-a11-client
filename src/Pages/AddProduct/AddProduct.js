c8028ab8ff77d8b8e9e29feaf1c0497fb82f4d90
Muzahidul Islam <mujahidul.core@gmail.com>
2022-05-12 20:39:15 +0600

    many changes update addedProduct and updatedProduct

--- src/Pages/AddProduct/AddProduct.js
+++ src/Pages/AddProduct/AddProduct.js
@@ -1,42 +1,60 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
 import './AddProduct.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AddProduct = () => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => {
-        console.log(data);
+
+    const handleAddProduct = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const price = event.target.price.value;
+        const supplierName = event.target.name.value;
+        const quantity = event.target.quantity.value;
+        const description = event.target.description.value;
+        const img = event.target.img.value;
+
+        const product = { name, price, supplierName, quantity, description, img };
+
         const url = `http://localhost:5000/product/`;
         fetch(url, {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(product)
         })
             .then(res => res.json())
-            .then(result => {
-                console.log(result);
+            .then(data => {
+                event.target.reset();
+                toast('Product Added');
             })
     };
     return (
         <div className='container form'>
             <h2 className='my-5 page-title'>Add New Product</h2>
 
-            <form className='w-50 mx-auto' onSubmit={handleSubmit(onSubmit)}>
+            <form className='w-50 mx-auto' onSubmit={handleAddProduct}  >
                 <label className='mb-2' htmlFor="">Product Name</label>
-                <input placeholder='Enter Your Product Name' {...register("name")} required />
+                <input placeholder='Enter Your Product Name' name='name' type={'text'} required />
+
                 <label className='mb-2' htmlFor=""> Price</label>
-                <input placeholder='Enter Price' type="number" {...register("price")} required />
+                <input placeholder='Enter Price' name='price' type="number" required />
+
                 <label className='mb-2' htmlFor=""> Supplier Name</label>
-                <input placeholder='Enter Supplier Name' {...register("supplierName")} required />
+                <input placeholder='Enter Supplier Name' name='supplierName' type={'text'} required />
+
                 <label className='mb-2' htmlFor=""> Quantity</label>
-                <input placeholder='Quantity' type="number" {...register("quantity")} required />
+                <input placeholder='Quantity' name='quantity' type="number" required />
+
                 <label className='mb-2' htmlFor=""> Description</label>
-                <textarea placeholder='Short Description' {...register("description")} required />
+                <textarea placeholder='Short Description' name='description' type={'text'} required />
+
                 <label className='mb-2' htmlFor=""> Photo URL</label>
-                <input placeholder='Image URL' {...register("img")} required />
+                <input placeholder='Image URL' name='img' type={'text'} required />
+
                 <input className='w-50 d-block mx-auto' style={{ backgroundColor: "#00896F", color: "#ffffff" }} type="submit" value="Add Product" />
+                <ToastContainer />
             </form>
         </div>
     );
--- src/Pages/UpdateProduct/UpdateProduct.js
+++ src/Pages/UpdateProduct/UpdateProduct.js
@@ -1,44 +1,67 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const UpdateProduct = () => {
     const { productId } = useParams();
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => {
-        console.log(data);
+
+    const handleUpdateProduct = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const price = event.target.price.value;
+        const supplierName = event.target.name.value;
+        const quantity = event.target.quantity.value;
+        const description = event.target.description.value;
+        const img = event.target.img.value;
+
+        const updatedProduct = { name, price, supplierName, quantity, description, img };
+
+
         const url = `http://localhost:5000/product/${productId}`
         fetch(url, {
             method: 'PUT',
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(updatedProduct)
         })
             .then(res => res.json())
-            .then(result => {
-                console.log(result);
+            .then(data => {
+                console.log(data)
+                event.target.reset();
+                toast('Product Updated');
             })
     };
     return (
         <div className='container form'>
             <h2 className='my-5 page-title'>Update Product </h2>
 
-            <form className='w-50 mx-auto' onSubmit={handleSubmit(onSubmit)}>
+            <form className='w-50 mx-auto' onSubmit={handleUpdateProduct}  >
                 <label className='mb-2' htmlFor="">Product Name</label>
-                <input placeholder='Enter Your Product Name' {...register("name")} required />
+                <input placeholder='Enter Your Product Name' name='name' type={'text'} required />
+
                 <label className='mb-2' htmlFor=""> Price</label>
-                <input placeholder='Enter Price' type="number" {...register("price")} required />
+                <input placeholder='Enter Price' name='price' type="number" required />
+
                 <label className='mb-2' htmlFor=""> Supplier Name</label>
-                <input placeholder='Enter Supplier Name' {...register("supplierName")} required />
+                <input placeholder='Enter Supplier Name' name='supplierName' type={'text'} required />
+
                 <label className='mb-2' htmlFor=""> Quantity</label>
-                <input placeholder='Quantity' type="number" {...register("quantity")} required />
+                <input placeholder='Quantity' name='quantity' type="number" required />
+
                 <label className='mb-2' htmlFor=""> Description</label>
-                <textarea placeholder='Short Description' {...register("description")} required />
+                <textarea placeholder='Short Description' name='description' type={'text'} required />
+
                 <label className='mb-2' htmlFor=""> Photo URL</label>
-                <input placeholder='Image URL' {...register("img")} required />
-                <input className='w-50 d-block mx-auto' style={{ backgroundColor: "#00896F", color: "#ffffff" }} type="submit" value="Update Product" />
+                <input placeholder='Image URL' name='img' type={'text'} required />
+
+                <input className='w-50 d-block mx-auto' style={{ backgroundColor: "#00896F", color: "#ffffff" }} type="submit" value="Updated Product" />
+                <ToastContainer />
             </form>
+
+
+
         </div>
     );
 };
